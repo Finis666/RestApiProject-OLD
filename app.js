98f9b52c2cbbfe46fba70a414042ba46eebad0fc
Finis666 <81584124+Finis666@users.noreply.github.com>
2022-06-03 14:49:46 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 require("./model/user");
-mongoose.connect("mongodb://localhost/HackerYouProject")
+mongoose.connect("mongoLink")
     .then(() =>{
         console.log("Connected to MongoDB")
     }).catch(()=>{
